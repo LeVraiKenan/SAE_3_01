9a383dddd77196b137742c357b812a532d7888f4
LeVraiKenan <kenan.keiser2@etu.univ-lorraine.fr>
2025-12-16 17:57:07 +0100

    Ajout de la serialization

---
+++ QDev/TribolloJfx/demo/src/main/java/tribollojfx/demo/SerializedTaskRepository.java
@@ -0,0 +1,33 @@
+package tribollojfx.demo;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializedTaskRepository {
+    private static final String FILE_NAME = "tasks.dat";
+
+    public void saveAll(List<Task> tasks) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(
+                new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(tasks);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Task> loadAll() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(
+                new FileInputStream(file))) {
+            return (List<Task>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
--- QDev/TribolloJfx/demo/src/main/java/tribollojfx/demo/Task.java
+++ QDev/TribolloJfx/demo/src/main/java/tribollojfx/demo/Task.java
@@ -3,9 +3,11 @@ package tribollojfx.demo;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.io.Serializable;
 
-public class Task {
+public class Task implements Serializable {
     private UUID id;
+    private UUID parentId;
     private String titre;
     private String description;
     private Statut statut;
@@ -22,6 +24,7 @@ public class Task {
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
         this.statut = Statut.A_FAIRE;
+        this.parentId = null;
     }
 
     public Task(String titre) {
@@ -30,6 +33,7 @@ public class Task {
     }
 
     public void addSousTask(Task task) {
+        task.parentId = this.id;
         this.sousTaches.add(task);
     }
 
@@ -41,12 +45,15 @@ public class Task {
     public LocalDateTime getDateDebut() { return dateDebut; }
     public LocalDateTime getDateFin() { return dateFin; }
     public ArrayList<Task> getSousTaches() { return sousTaches; }
+    public UUID getParentId() { return parentId; }
+
 
     public void setTitre(String titre) { this.titre = titre; }
     public void setDescription(String description) { this.description = description; }
     public void setPriorite(Priorite priorite) { this.priorite = priorite; }
     public void setDateDebut(LocalDateTime dateDebut) { this.dateDebut = dateDebut; }
     public void setDateFin(LocalDateTime dateFin) { this.dateFin = dateFin; }
+    public void setParentId(UUID parentId) { this.parentId = parentId; }
 
     public void changerStatut(Statut statut) { this.statut = statut; }
 
