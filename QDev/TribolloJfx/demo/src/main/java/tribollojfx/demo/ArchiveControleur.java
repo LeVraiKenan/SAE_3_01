5409698accf209efa6d43137d00aa6fd9e0cca28
Alexandre BEKRI <e44703u@etu.univ-lorraine.fr>
2025-12-17 13:58:02 +0100

    ArchiveControleur : Suppression de la liste locale et délégation au Model

--- QDev/TribolloJfx/demo/src/main/java/tribollojfx/demo/ArchiveControleur.java
+++ QDev/TribolloJfx/demo/src/main/java/tribollojfx/demo/ArchiveControleur.java
@@ -4,21 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArchiveControleur {
-    private List<Task> archivees;
+    private TaskModel model; // Tout est gérée par une seule liste du model à présent
 
-    public ArchiveControleur() {
-        this.archivees = new ArrayList<>();
+    public ArchiveControleur(TaskModel model) {
+        this.model = model;
     }
 
     public void archiver(Task t){
-        archivees.add(t);
+        model.updateTaskStatut(t.getId(), Statut.ARCHIVEE);
     }
 
     public void restaurer(Task t){
-        archivees.remove(t);
+        // On la rebascule sur A_FAIRE par défaut
+        // (Note : Ici, nous perdons l'état précédent, exemple si elle était EN_COURS)
+        model.updateTaskStatut(t.getId(), Statut.A_FAIRE);
     }
 
     public List<Task> getArchives() {
-        return archivees;
+        return model.getTachesByStatut(Statut.ARCHIVEE);
     }
 }
